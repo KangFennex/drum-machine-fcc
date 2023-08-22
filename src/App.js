918d17ca104e368a09cbb873528d3acb404a9664
kangmeta <109540950+kangmeta@users.noreply.github.com>
2023-08-22 09:54:53 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { useState } from "react";
+import { GiMusicalNotes } from "react-icons/gi";
+import { AiOutlinePoweroff } from "react-icons/ai";
+import { sounds } from "./config";
 
 function App() {
+  const [soundBite, setSoundBite] = useState("");
+  const [isOff, setIsOff] = useState(false);
+  const [volume, setVolume] = useState(50);
+  const [isDraggingVolume, setIsDraggingVolume] = useState(false);
+
+  const handleShowDisplay = (title) => {
+    setSoundBite(title);
+  };
+
+  const playSound = (sound) => {
+    const audio = new Audio(sound);
+    audio.volume = volume / 100;
+    audio.play();
+  };
+
+  const handleDrumPadClick = (source) => {
+    playSound(source);
+  };
+
+  const handleIsOff = () => {
+    setIsOff(!isOff);
+    document.getElementById("display").innerHTML = "";
+  };
+
+  const handleVolumeDragStart = () => {
+    setIsDraggingVolume(true);
+  };
+
+  const handleVolumeDrag = (event) => {
+    if (isDraggingVolume) {
+      const volumePosition =
+        event.clientX - event.target.getBoundingClientRect().left;
+      const newVolume = Math.min(
+        Math.max(
+          Math.round((volumePosition / event.target.offsetWidth) * 100),
+          0
+        ),
+        100
+      );
+      setVolume(newVolume);
+      setSoundBite(`Volume: ${newVolume}`);
+    }
+  };
+
+  const handleVolumeDragEnd = () => {
+    setIsDraggingVolume(false);
+  };
+
+  const handleVolumeChange = (event) => {
+    const newVolume = Math.min(Math.max(parseInt(event.target.value), 0), 100);
+    setVolume(newVolume);
+    setSoundBite(`Volume: ${newVolume}`);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="dm-container">
+        <div id="drum-machine" className="dm-container__dm">
+          <GiMusicalNotes size={40} className="dm-container__dm__music-icon" />
+          <div id="display" className="dm-container__dm__display">
+            {soundBite}
+          </div>
+          <div className="dm-container__dm__drum-pads">
+            {sounds.map((sound) => (
+              <div
+                key={sound.title}
+                id="drum-pad"
+                className="dm-container__dm__drum-pads__drum-pad"
+                onClick={() => {
+                  if (!isOff) {
+                    handleDrumPadClick(sound.source);
+                    handleShowDisplay(sound.title);
+                  }
+                }}
+              >
+                {sound.id}
+              </div>
+            ))}
+          </div>
+          <div className="dm-container__dm__control-pad">
+            <AiOutlinePoweroff
+              size={35}
+              className="dm-container__dm__control-pad__turn-off-icon"
+              style={{ color: isOff ? "#e74c3c" : "black" }}
+              onClick={handleIsOff}
+            />
+            <div className="dm-container__dm__control-pad__volume">
+              <input
+                type="range"
+                min="0"
+                max="100"
+                value={volume}
+                onChange={isOff ? null : handleVolumeChange}
+                onMouseDown={isOff ? null : handleVolumeDragStart}
+                onMouseMove={isOff ? null : handleVolumeDrag}
+                onMouseUp={isOff ? null : handleVolumeDragEnd}
+                onMouseLeave={isOff ? null : handleVolumeDragEnd}
+                className="dm-container__dm__control-pad__volume__volume-slider"
+              />
+            </div>
+          </div>
+
+          <h2>
+            Coded by <a href="https://github.com/KangFennex">Kangkm</a>
+          </h2>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,57 @@
+import q from "./sounds/Heater-1.mp3";
+import w from "./sounds/Heater-2.mp3";
+import e from "./sounds/Heater-3.mp3";
+import a from "./sounds/Heater-4.mp3";
+import s from "./sounds/Heater-6.mp3";
+import z from "./sounds/Kick_n_Hat.mp3";
+import x from "./sounds/RP4_KICK_1.mp3";
+import c from "./sounds/Cev_H2.mp3";
+import d from "./sounds/Dsc_Oh.mp3";
+
+export const sounds = [
+  {
+    title: "Heater 1",
+    id: "Q",
+    source: q,
+  },
+  {
+    title: "Heater 2",
+    id: "W",
+    source: w,
+  },
+  {
+    title: "Heater 3",
+    id: "E",
+    source: e,
+  },
+  {
+    title: "Heater 4",
+    id: "A",
+    source: a,
+  },
+  {
+    title: "Clap",
+    id: "S",
+    source: s,
+  },
+  {
+    title: "Open HH",
+    id: "D",
+    source: d,
+  },
+  {
+    title: "Kick n'Hat",
+    id: "Z",
+    source: z,
+  },
+  {
+    title: "Kick",
+    id: "X",
+    source: x,
+  },
+  {
+    title: "Closed HH",
+    id: "C",
+    source: c,
+  },
+];
